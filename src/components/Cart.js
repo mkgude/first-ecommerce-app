3d25d7a35294d4663b14bce7600ac6a0db40caed
Marian Gude <mkgude@gmail.com>
2020-11-17 15:28:44 -0600

    added modal of products to display in a larger form when selecting a item

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import formatCurrency from '../util';
+import Fade from "react-reveal/Fade"
 
 class Cart extends Component {
     constructor(props){
@@ -36,6 +37,7 @@ class Cart extends Component {
         )}
                 <div>
                     <div className="cart">
+                        <Fade left cascade>
                         <ul className="cart-items">
                             {cartItems.map((item) => (
                             <li key={item.id}>
@@ -57,6 +59,7 @@ class Cart extends Component {
                             </li>
                             ))}
                         </ul>
+                        </Fade>
                     </div>
                     <div className="cart">
                         <div className="total">
@@ -67,6 +70,7 @@ class Cart extends Component {
                     </div>
                 </div>
                 {this.state.showCheckout && (
+                    <Fade right cascade>
                      <div className="cart">
                          <form onSubmit={this.createOrder}>
                              <ul className="form-container">
@@ -84,6 +88,7 @@ class Cart extends Component {
                              </ul>
                          </form>
                      </div>
+                     </Fade>
                 )}
             </div>
         )
